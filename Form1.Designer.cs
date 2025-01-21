e92270370d2699afa758f39abcc72ab2cb107969
hee082024kh <hee082024kh@gmail.com>
2025-01-21 12:23:41 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace TetrisV2;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        pictureBox1 = new System.Windows.Forms.PictureBox();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        SuspendLayout();
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Location = new System.Drawing.Point(12, 8);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new System.Drawing.Size(776, 632);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 647);
+        Controls.Add(pictureBox1);
+        Text = "Form1";
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+
+    #endregion
+}
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+namespace TetrisV2;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+            
+        loadCanvas();
+    }
+        
+    Bitmap canvasBitmap;
+    Graphics canvasGraphics;
+    int canvasWidth = 15;
+    int canvasHeight = 20;
+    int[,] canvasDotArray;
+    int dotSize = 20;
+
+    private void loadCanvas()
+    {
+        // Resize the picture box based on the dotsize and canvas size
+        pictureBox1.Width = canvasWidth * dotSize;
+        pictureBox1.Height = canvasHeight * dotSize;
+
+        // Create Bitmap with picture box's size
+        canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+        canvasGraphics = Graphics.FromImage(canvasBitmap);
+
+        canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+
+        // Load bitmap into picture box
+        pictureBox1.Image = canvasBitmap;
+            
+        // Initialize canvas dot array. by default all elements are zero
+        canvasDotArray = new int[canvasWidth, canvasHeight];
+    }
+}
