589b5bbd436264ef27e9a52771b0d620085970de
hee082024kh <hee082024kh@gmail.com>
2025-01-23 10:32:17 +0100

    Added Turn and Rollback methods

--- Form1.cs
+++ Form1.cs
@@ -73,6 +73,40 @@ public partial class Form1 : Form
         public int Width;
         public int Height;
         public required int[,] Dots;
+
+        private int[,] backupDots;
+
+        public void Turn()
+        {
+            // back the dots values into backup dots
+            // so that it can be simply used for rolling back
+            backupDots = Dots;
+            
+            Dots = new int[Height, Width];
+
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Dots[i, j] = backupDots[Height - 1 - j, i];
+                }
+            }
+
+            var temp = Width;
+            Width = Height;
+            Height = temp;
+        }
+        
+        // the rolling back occurs when player rotating the shape
+        // but it will touch other shapes and needs to be rolled back
+        public void Rollback()
+        {
+            Dots = backupDots;
+            
+            var temp = Width;
+            Width = Height;
+            Height = temp;
+        }
     }
 
     // Holds the different shapes
